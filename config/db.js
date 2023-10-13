d08ffc8d0c9890d5d04c02bb878a0a272dafcf11
DeepakPun <90846395+DeepakPun@users.noreply.github.com>
2023-10-13 16:55:22 -0400

    Basic routes setup. No Controllers yet though.

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoDevURI')
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db)
+
+    console.log(`Connected to Mongo`.cyan.underline)
+  } catch (error) {
+    console.error(`Error connecting to Mongo`.red)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const connectDB = require('./config/db')
+require('colors')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+// Connect to database
+connectDB()
+
 app.get('/', (req, res) => {
   res.send('API is up and running')
 })
+
+// Define and Mount routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`)
